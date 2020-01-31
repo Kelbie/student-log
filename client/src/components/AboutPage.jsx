2f4cbbd4faf996c4ebab81626c91dc319b5b93bb
KevinKelbie <notkelbie@outlook.com>
2020-01-31 18:07:21 +0000

    add about page

---
+++ client/src/components/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function AboutPage(props) {
+    return <div {...props}>
+        <h1>About</h1>
+        <p>This website is a student portal which allows students to:</p>
+        <ul>
+            <li>Build their CV</li>
+            <li>Find internships and graduate jobs</li>
+            <li></li>
+        </ul>
+    </div>
+}
+
+export default styled(AboutPage)`
+    color: white;
+`;
