8b42317465460a70f6b422d8e2c66e479849f734
KevinKelbie <notkelbie@outlook.com>
2020-02-07 12:16:33 +0000

    remove hardcoded timetable

--- models/timetable.js
+++ models/timetable.js
@@ -2,15 +2,11 @@ import ical from 'node-ical';
 
 exports.fromURL = async function(url) {
   return await new Promise((resolve, reject) => {
-    return ical.fromURL(
-      'https://timetables.rgu.ac.uk/iCal/ical/ical/QF2DXRCL547645/schedule.ics',
-      {},
-      function(err, data) {
-        if (err) {
-          reject(err);
-        }
-        resolve(data);
+    return ical.fromURL(url, {}, function(err, data) {
+      if (err) {
+        reject(err);
       }
-    );
+      resolve(data);
+    });
   });
 };
