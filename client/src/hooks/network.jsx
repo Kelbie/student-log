ca38bfc46ab1664dc9f035a0f266ea0e3ceb6aa6
KevinKelbie <notkelbie@outlook.com>
2020-02-14 15:39:49 +0000

    add useNetwork hook

---
+++ client/src/hooks/network.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function useNetwork() {
+  const [isOnline, setNetwork] = useState(window.navigator.onLine);
+
+  const updateNetwork = () => {
+    setNetwork(window.navigator.onLine);
+  };
+
+  useEffect(() => {
+    window.addEventListener('offline', () => setNetwork(window.navigator.onLine));
+    window.addEventListener('online', () => setNetwork(window.navigator.onLine));
+
+    return () => {
+      window.removeEventListener('offline', updateNetwork);
+      window.removeEventListener('online', updateNetwork);
+    };
+  }, []);
+
+  return { isOnline };
+}
+
+export default useNetwork;
